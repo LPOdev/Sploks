afa22a99b5752a5c6c0205588e02c5332c27ed1d
Luís Pedro Pinheiro <47849666+LPOdev@users.noreply.github.com>
2022-05-16 10:49:48 +0200

    T34 Classe Duration

--- sploks/model/duration.py
+++ sploks/model/duration.py
@@ -1,14 +1,6 @@
 from model import crud
 
 class Duration:
-
-    def load(self, id):
-        state = crud.selectOneWithParams("*", "gearstates", f"Where id = {id}")
-
-        self.id = state['id']
-        self.code = state['code']
-        self.description = state['description']
-
     @staticmethod
     def all():
         return crud.selectWithParams("*", "durations")
